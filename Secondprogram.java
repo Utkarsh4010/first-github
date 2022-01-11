a9eee337c8a40a01e158e7b7b37cae7598a9878b
Utkarsh4010 <utkarshsuryawanshi704@gmail.com>
2022-01-11 13:44:44 +0530

    git learning

---
+++ Secondprogram.java
@@ -0,0 +1,21 @@
+public class Firstprogram
+{
+   public static void main (String[] args)
+{
+System.out.println("hello , this is my first java program");
+ }
+}
+class Secondprogram
+{
+public static void main (String[] args)
+{
+System.out.println("hello ,this is my second java program");
+}
+}
+class Thirdprogram
+{
+public Static void main (String[] args)
+{
+System.out.println("hello, this is my third java program");
+}
+}
